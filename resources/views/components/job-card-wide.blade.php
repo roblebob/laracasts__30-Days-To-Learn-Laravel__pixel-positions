6daf5a6cb1587400767f4745407282c9874b8df4
roblebob <robert.koppisch@gmail.com>
2024-11-15 14:23:31 +0100

    day 28: Final Project: Blade and Tailwind Techniques for Your Laravel Views  DONE

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,21 @@
+<x-panel class="flex flex-row gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="mt-3 font-bold text-xl group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="mt-auto text-sm text-gray-400">Fulltime - From $60,000</p>
+
+    </div>
+
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+
+
+</x-panel>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,15 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                "hanken-grotesk": ['Hanken Grotesk', 'sans-serif'],
             },
             colors: {
                 "black": "#060606",
-            }
+            },
+            fontSize: {
+                "2xs": ".625rem",
+                "3xs": ".5rem",
+            },
         },
     },
     plugins: [],
